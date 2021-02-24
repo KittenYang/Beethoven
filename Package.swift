cd5e6b9c0369b37115b36d86ba472224eb17bab9
KittenYang <kittenyang@icloud.com>
2021-02-24 22:09:58 +0800

    support spm

--- Package.swift
+++ Package.swift
@@ -1,8 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Beethoven",
-  dependencies: [
-    .Package(url: "https://github.com/vadymmarkov/Pitchy.git", majorVersion: 2),
-  ]
+    name: "Beethoven",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "Beethoven",
+            targets: ["Beethoven"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vadymmarkov/Pitchy.git", from: "2"),
+    ],
+    targets: [
+        .target(
+            name: "Beethoven",
+            path: "Source")
+    ]
 )
+
