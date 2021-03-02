65a3ed574265e8089fdfdad3a7ea1a4fa99e7d85
KittenYang <kittenyang@icloud.com>
2021-03-02 23:05:02 +0800

    mark public

--- Source/Estimation/EstimationFactory.swift
+++ Source/Estimation/EstimationFactory.swift
@@ -1,27 +1,28 @@
 public final class EstimationFactory {
-  public init() {}
-  public func create(_ strategy: EstimationStrategy) -> Estimator {
-    let estimator: Estimator
-
-    switch strategy {
-    case .quadradic:
-      estimator = QuadradicEstimator()
-    case .barycentric:
-      estimator = BarycentricEstimator()
-    case .quinnsFirst:
-      estimator = QuinnsFirstEstimator()
-    case .quinnsSecond:
-      estimator = QuinnsSecondEstimator()
-    case .jains:
-      estimator = JainsEstimator()
-    case .hps:
-      estimator = HPSEstimator()
-    case .yin:
-      estimator = YINEstimator()
-    default:
-      estimator = MaxValueEstimator()
+    public init() {}
+    public func create(_ strategy: EstimationStrategy) -> Estimator {
+        let estimator: Estimator
+        
+        switch strategy {
+        case .quadradic:
+            estimator = QuadradicEstimator()
+        case .barycentric:
+            estimator = BarycentricEstimator()
+        case .quinnsFirst:
+            estimator = QuinnsFirstEstimator()
+        case .quinnsSecond:
+            estimator = QuinnsSecondEstimator()
+        case .jains:
+            estimator = JainsEstimator()
+        case .hps:
+            estimator = HPSEstimator()
+        case .yin:
+            estimator = YINEstimator()
+        default:
+            estimator = MaxValueEstimator()
+        }
+        
+        return estimator
     }
-
-    return estimator
-  }
 }
+
